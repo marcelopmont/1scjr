610c2b406c610a42ebe3b962bb44e40490554937
Marcelo P Montanher <marcelo.montanher@trela.com.br>
2023-03-04 10:59:34 -0300

    Challenge solution

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -30,28 +30,68 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.teal,
       body: SafeArea(
-        child: Column(
-          children: [
-            Image.network(
-              'https://pbs.twimg.com/media/FKNlhKZUcAEd7FY?format=jpg&name=4096x4096',
-            ),
-            SizedBox(height: 16),
-            CircleAvatar(
-              backgroundImage: NetworkImage(
-                  'https://pbs.twimg.com/media/FKNlhKZUcAEd7FY?format=jpg&name=4096x4096'),
-              radius: 120,
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Flutter is Awesome',
-              style: TextStyle(
-                fontSize: 42,
-                color: Colors.white,
-                fontWeight: FontWeight.w800,
-                fontFamily: 'mynerve',
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage(
+                  'assets/images/profile.jpg',
+                ),
+                radius: 90,
               ),
-            ),
-          ],
+              SizedBox(
+                height: 16,
+                width: double.infinity,
+              ),
+              Text(
+                'Marcelo P Montanher',
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800,
+                  fontFamily: 'mynerve',
+                ),
+              ),
+              SizedBox(height: 32),
+              Material(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                elevation: 8,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_android,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '11 12345-6789',
+                    style: TextStyle(
+                      color: Colors.teal,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Material(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                elevation: 8,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'teste@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
