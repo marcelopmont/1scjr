0cb9106000d63e62a411b7a89e399a5a345e879c
Marcelo P Montanher <marcelo.montanher@trela.com.br>
2023-03-04 12:10:40 -0300

    Example using whatsapp

--- lib/main.dart
+++ lib/main.dart
@@ -65,10 +65,17 @@ class MyHomePage extends StatelessWidget {
                     : Icons.phone_iphone,
                 text: '11 12345-6789',
                 onPressed: () {
+<<<<<<< Updated upstream
                   launchUrl(Uri(
                     scheme: 'tel',
                     path: '+55 11 12345-6789',
                   ));
+=======
+                  final url = Uri.parse(
+                    'whatsapp://send?phone=11123456789',
+                  );
+                  launchUrl(url);
+>>>>>>> Stashed changes
                 },
               ),
               SizedBox(height: 16),
